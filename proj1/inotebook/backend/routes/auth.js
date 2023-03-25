c191f3c428e2d0023eb534863011eb40b23e56ae
sailendrachettri <sailendra9083@gmail.com>
2023-03-25 21:16:25 +0530

    leacture 46 complected

--- proj1/inotebook/backend/routes/auth.js
+++ proj1/inotebook/backend/routes/auth.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const User = require("../models/User")
+const { body, validationResult } = require('express-validator')
 
-router.post('/', (req, res) => {
+router.post('/', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    body('name').isLength({ min: 3 })
+], (req, res) => {
 
-    console.log(req.body);
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+        return res.status(400).json({ error: error.array() })
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: 'Please enter a unique value for email' })
+        })
 })
 
 module.exports = router
