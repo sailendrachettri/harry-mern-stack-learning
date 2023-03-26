ace67d94b484f57780a17a05e926a1d2450e6c05
sailendrachettri <sailendra9083@gmail.com>
2023-03-26 12:39:16 +0530

    video 47 complected

--- proj1/inotebook/backend/db.js
+++ proj1/inotebook/backend/db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 const connectToMongo = () => {
-    // mongoose.connect(mongoURI)
-    // or
     mongoose.connect(mongoURI)
 }
 
--- proj1/inotebook/backend/routes/auth.js
+++ proj1/inotebook/backend/routes/auth.js
@@ -1,12 +1,16 @@
+const bcrypt = require('bcryptjs')
 const express = require('express')
 const router = express.Router()
 const User = require("../models/User")
 const { body, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+
+const JWT_TOKEN = 'jwt@token!.e@secret'
 
 router.post('/createuser', [
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }),
-    body('name').isLength({ min: 3 })
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Password length should be greater than 5').isLength({ min: 5 }),
+    body('name', 'Enter a valid name').isLength({ min: 3 })
 ], async (req, res) => {
 
     const error = validationResult(req)
@@ -20,13 +24,25 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Email already used" })
         }
+
+        const salt = await bcrypt.genSalt(10)
+        const secrectPassword = await bcrypt.hash(req.body.password, salt)
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secrectPassword,
         })
 
-        res.json(user)
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const auth_token = jwt.sign(data, JWT_TOKEN)
+
+        res.json({ auth_token })
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Something went wrong :(")
