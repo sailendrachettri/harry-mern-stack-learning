b68cc23a6bfb974a22daabce6fbda57bd4615a9b
sailendrachettri <sailendra9083@gmail.com>
2023-03-28 20:39:51 +0530

    lecture 56 complected

--- proj1/inotebook/src/App.js
+++ proj1/inotebook/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
       <>
-        <h1>Lets do the frontend :)</h1>
+        <Router >
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/user' element={<User />} />
+          </Routes>
+        </Router>
       </>
     </div>
   );
