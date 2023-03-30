24f841886efb4012bcdc20c0e8983f8e18c677d1
sailendrachettri <sailendra9083@gmail.com>
2023-03-30 11:11:01 +0530

    lecture 65 complected

--- proj1/inotebook/backend/index.js
+++ proj1/inotebook/backend/index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db')
 const express = require('express')
+const cors = require('cors')
 
 connectToMongo()
 
@@ -8,6 +9,7 @@ const port = 5000
 
 // middleware
 app.use(express.json())
+app.use(cors())
 
 // Available routes
 app.use('/api/auth', require('./routes/auth.js'))
